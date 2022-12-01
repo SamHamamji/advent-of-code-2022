e5d9fc81688c5a9bfde6171bfb61bc8c5db4335c
SamHamamji <samhamamji0@gmail.com>
2022-12-02 00:04:31 +0200

    Add Day 1 Part 1

--- src/day1/index.ts
+++ src/day1/index.ts
@@ -1,8 +1,23 @@
 import * as fs from 'fs';
 
 function main() {
-    const data = fs.readFileSync("./src/day1/input.txt", 'ascii');
-    const inventory = data.split("\n\n").map((array) => array.trim().split("\n"));
+    const data = fs.readFileSync(
+        "./src/day1/input.txt",
+        { encoding: 'ascii', flag: 'r' }
+    );
+    const inventory = data.split("\n\n").map((array) => array.trim().split("\n").map((element) => parseInt(element)));
+
+    let max = -Infinity;
+    let sum: number;
+    inventory.forEach(elf => {
+        sum = 0;
+        elf.forEach(food => {
+            sum += food;
+        });
+        max = (max >= sum) ? max : sum;
+    });
+
+    console.log(`The max of the calories held by an elf is: ${max}`);
 }
 
 main();
