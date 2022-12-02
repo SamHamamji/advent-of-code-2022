55d420ffafdc30223b7f7bc63d5a28cc01f7dcd1
SamHamamji <samhamamji0@gmail.com>
2022-12-02 14:24:34 +0200

    Check if day input is valid

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,25 @@
-const day = process.argv.slice(2)[0];
+import * as fs from 'fs';
+
+const firstDay = 1;
+const lastDay = 25;
+
+const args = process.argv.slice(2);
+const day = parseInt(args[0]);
+
+if (Number.isNaN(day)) {
+    const message = `Input a day between ${firstDay} and ${lastDay} inclusive`;
+    throw new Error(message);
+}
+
+if (!(day >= firstDay && day <= lastDay)) {
+    const message = `${day} is not a valid day`;
+    throw new Error(message);
+}
+
+const inputFile = `./src/day${day}/input.txt`;
+if (!fs.existsSync(inputFile)) {
+    const message = `${day} is not posted yet`;
+    throw new Error(message);
+}
 
 import(`./day${day}/index.ts`);
